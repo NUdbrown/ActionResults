7f7fcc6c6b0c47907bc1580d3c4f3c61a3cf3437
Dia Brown <dbrown1903@outlook.com>
2015-04-30 14:43:30 -0600

    Completed Project

---
+++ ActionResults/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Runtime.Serialization;
+using ActionResults.Models;
+using System.IO;
+using System.Xml;
+
+namespace ActionResults.Controllers
+{
+    public class HomeController : Controller
+    {
+        Character character = new Character();
+        
+        public ActionResult Index()
+        {
+            return View(character);
+        }
+
+      
+        public ActionResult Plain()
+        {   
+            return Content(character.ToString());
+        }
+
+        public ActionResult XML()
+        {
+            var serializer = new DataContractSerializer(typeof(Character));
+            string xmlString;
+            using (var sw = new StringWriter())
+            {
+                using (var writer = new XmlTextWriter(sw))
+                {
+                    writer.Formatting = Formatting.Indented; // indent the Xml so it's human readable
+                    serializer.WriteObject(writer, character);
+                    writer.Flush();
+                    xmlString = sw.ToString();
+                    return this.Content(xmlString, "text/xml");
+                }
+            }
+            
+        }
+
+        public ActionResult JSON()
+        {
+            return Json(character, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Error()
+        {
+            return HttpNotFound();
+        }
+
+        public ActionResult Download()
+        {
+            var imgPath = Server.MapPath("~/Images/sims4background.jpg");
+            Response.AddHeader("Content-Disposition", "attachment;filename=Sims4Background.png");
+            Response.WriteFile(imgPath);
+            Response.End();
+            return null;
+        }
+
+    }
+}
---
+++ ActionResults/Models/Character.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+
+namespace ActionResults.Models 
+{
+    [DataContract]
+    public class Character
+    {
+        [DataMember]
+        public string Name = "Jessica Rabbit";
+
+        [DataMember]
+        public int Level = 25;
+
+        [DataMember]
+        public int HealthPoints = 100;
+
+        [DataMember]
+        public Dictionary<string, int> Attributes = new Dictionary<string,int>(){
+                {"IQ", 24},
+                {"ME", 10},
+                {"MA", 16},
+                {"PS", 8},
+                {"PP", 20},
+                {"PE", 3},
+                {"Pb", 30},
+                {"Spd", 5}
+        };
+
+        public override string ToString()
+        {
+            string temp = " ";
+            foreach(KeyValuePair<string,int> att in Attributes){
+                temp += "[" + att.Key + ": " + att.Value + "]";
+            }
+
+            return "[Name: " + Name + "] [Level: " + Level + "] [Health Points: " + HealthPoints + "] [Attributes: " + temp + " ]" ;
+        }
+
+       
+
+    }
+}
